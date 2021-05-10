84846840a073f77536be0cce90ee1ac66f156fb7
kakeru14 <kakeru.otani@rakus-partners.co.jp>
2021-05-10 16:33:55 +0900

    コメント

--- product-a/src/components/Shopping-cart.vue
+++ product-a/src/components/Shopping-cart.vue
@@ -1,5 +1,24 @@
 <template>
-  <div>
-      大谷担当のショッピングカートです
+<div>
+  <!-- <div>
+      <h2>ショッピングカート</h2>
+      <li v-for="product in products" :key="product">
+          {{ product.title }} - {{ product.price }}
+      </li>
+
+      <p><button :disabled="!cartProducts.length" @click="checkout(cartProducts)">Checkout</button></p>
+  </div> -->
   </div>
 </template>
+<script>
+// import { mapState } from '@vue/composition-api'
+
+// export default {
+//     computed:{
+
+
+
+//         ...mapState(["products"])
+//     }
+// }
+</script>
